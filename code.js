1e0220794181d4fb1b0972fd64de43d85ceb4e09
natebobsee <nate.bob.see@gmail.com>
2024-01-31 14:40:59 -0800

    first commit

---
+++ code.js
@@ -0,0 +1,5 @@
+//using arithmatic to generate the codes
+const code1 = 5*2;
+const code2 = 30+10;
+const code3 = 40-1;
+const str1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
